713d341b350a49cd36ecbadb314488a54454e0e7
Marcelo Roberto Jimenez <marcelo.jimenez@gmail.com>
2024-03-03 14:07:29 -0300

    Add cpp, javascript and python to LSP

--- init.lua
+++ init.lua
@@ -781,7 +781,7 @@ require('lazy').setup {
 
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+        ensure_installed = { 'bash', 'c', 'cpp', 'html', 'javascript', 'lua', 'markdown', 'python', 'vim', 'vimdoc' },
         -- Autoinstall languages that are not installed
         auto_install = true,
         highlight = { enable = true },

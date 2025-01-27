4467dda4197d54ea202b658ce6a3e6315ca04ecb
Marcelo Roberto Jimenez <marcelo.jimenez@gmail.com>
2025-01-27 14:56:35 -0300

    Debug plugin needs neo

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -11,6 +11,9 @@ return {
   'mfussenegger/nvim-dap',
   -- NOTE: And you can specify dependencies as well
   dependencies = {
+    -- Needs nio
+    'nvim-neotest/nvim-nio',
+
     -- Creates a beautiful debugger UI
     'rcarriga/nvim-dap-ui',
 

ee33ad939a175298d463de7236ce92faa4b70bc8
Marcelo Roberto Jimenez <marcelo.jimenez@gmail.com>
2025-05-14 00:32:54 -0300

    clangd config

--- init.lua
+++ init.lua
@@ -671,7 +671,9 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        clangd = {},
+        clangd = {
+          --cmd = {'/usr/bin/clangd', '--background-index', '--clang-tidy', '--log=verbose'},
+        },
         -- gopls = {},
         pyright = {},
         -- rust_analyzer = {},
